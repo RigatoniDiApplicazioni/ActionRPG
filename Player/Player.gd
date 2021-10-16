8457bc7c732b3c9ec2dff4e3e9eff0d465de6903
RichterBea <richter.falkenstein@web.de>
2021-10-16 11:58:04 +0200

    add roll animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,8 +1,9 @@
 extends KinematicBody2D
 
-const ACCELERATION = 10
-const MAX_SPEED = 100
-const FRICTION = 0	# an wand langlaufen --> langsam
+const ACCELERATION = 500
+const MAX_SPEED = 80
+const ROLL_SPEED = 120
+const FRICTION = 500	# an wand langlaufen --> langsam
 
 enum {
 	MOVE,
@@ -12,6 +13,7 @@ enum {
 
 var state = MOVE
 var velocity = Vector2.ZERO
+var roll_vector = Vector2.LEFT
 
 onready var animationPlayer = $AnimationPlayer
 	# ..oder --> var animationPlayer = null
@@ -29,9 +31,9 @@ func _physics_process(delta):
 		MOVE:
 			move_state(delta)
 		ROLL:
-			pass
+			roll_state()
 		ATTACK:
-			attack_state(delta)
+			attack_state()
 
 func move_state(delta):
 	var input_vector = Vector2.ZERO
@@ -40,23 +42,40 @@ func move_state(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector		# sonst rollt der char auf der stelle
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = input_vector
 	else:
 		animationState.travel("Idle")
 		velocity = Vector2.ZERO
-		
-	velocity = move_and_slide(velocity * MAX_SPEED)
 
+	move()
+	
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
-		
-func attack_state(delta):
+	
+	if Input.is_action_just_pressed("roll"):
+		state = ROLL
+
+func move():
+	velocity = move_and_slide(velocity * MAX_SPEED)
+	
+func roll_state():
+	velocity = roll_vector * 1.1
+	animationState.travel("Roll")
+	move()
+	
+func attack_state():
 	velocity = Vector2.ZERO
 	animationState.travel("Attack")
-
+	
 func attack_animation_finished():
+	state = MOVE
+
+func roll_animation_finished():
+	velocity = velocity * 0.8
 	state = MOVE
